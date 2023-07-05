b8549d7f5195c34e808fd8a12ad97605d62b3243
Puranjay Prashanth <51010162+pprasha@users.noreply.github.com>
2023-07-04 21:53:38 -0500

    init

--- Project UCode/src/index.js
+++ Project UCode/src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC0paQLoPCLrKoUKsg1d4qgpvqQdUNEAj8",
+//   authDomain: "project-ucode-website.firebaseapp.com",
+//   projectId: "project-ucode-website",
+//   storageBucket: "project-ucode-website.appspot.com",
+//   messagingSenderId: "691058164",
+//   appId: "1:691058164:web:9c329967a1a13fa128b5a1",
+//   measurementId: "G-R6ZKE5CWX3"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
